33aeeee642e077f812c928181e72ce9a0301991c
IsabelCollado <icolladoherrera@gmail.com>
2023-06-15 18:44:55 +0200

    Add changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,9 @@
 // Fichero src/components/App.js
 
-import { Route, Routes } from 'react-router-dom';
+//import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <div>
-      <h2>Este título aparece siempre</h2>
-
-      <Routes>
-        <Route
-          path="/contacto"
-          element={
-            <h2>
-              Este título solo aparece cuando la usuaria entra en la página de
-              contacto
-            </h2>
-          }
-        />
-      </Routes>
-
-      <nav>
-        <ul>
-          <li>
-            <a href="/#/">Ir al inicio</a>
-          </li>
-          <li>
-            <a href="/#/contacto">Ir a contacto</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
