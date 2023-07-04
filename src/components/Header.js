3005ef1110274fc6a0ef7e36f9a5e98e959ead75
IsabelCollado <icolladoherrera@gmail.com>
2023-07-04 12:29:24 +0200

    Add hover the links

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,21 +6,21 @@ const Header = () => {
     <header className="header">
       <nav className="nav-container">
         <NavLink to="/" className="nav-header">
-          <h1 className="title">HOME</h1>
+          <h1 className="title">Home</h1>
         </NavLink>
         <ul className="ul-header">
           <li className="li-about">
-            <NavLink to="/aboutme" className="nav-link">
+            <NavLink to="/aboutme" className="underline-link">
               About Me
             </NavLink>
           </li>
           <li className="li-projects">
-            <NavLink to="/projects" className="nav-link">
+            <NavLink to="/projects" className="underline-link">
               Projects
             </NavLink>
           </li>
           <li className="li-contact">
-            <NavLink to="/contact" className="nav-link">
+            <NavLink to="/contact" className="underline-link">
               Contact
             </NavLink>
           </li>
