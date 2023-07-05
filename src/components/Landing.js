f58e80dc759f25a57d7996257695affd6de132c0
IsabelCollado <icolladoherrera@gmail.com>
2023-07-05 15:16:16 +0200

    Add landing--p

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,19 +3,28 @@ import '../styles/layouts/landing.scss';
 
 const Landing = () => {
   return (
-    <section className="landing-section">
-      <div className="landing-container">
-        <img
-          className="img_logo"
-          src={logo}
-          alt="Logo_image"
-          title="logo_Isa"
-        />
-      </div>
-      <div className="hero__profession--line border__up"></div>
-      <h2 className="hero__profession">Full-Stack Developer</h2>
-      <div className="hero__profession--line border__down"></div>
-    </section>
+    <>
+      <section className="landing-section">
+        <div className="landing-container">
+          <img
+            className="img_logo"
+            src={logo}
+            alt="Logo_image"
+            title="logo_Isa"
+          />
+        </div>
+        <p className="landing-section--p">
+          I consider myself a strong, hard-working and persevering person like
+          the earth element. I am persevering in everything I set my mind to. I
+          am a dog lover, especially the Rottweiler breed.
+        </p>
+      </section>
+      <section>
+        <div className="hero__profession--line border__up"></div>
+        <h2 className="hero__profession">Full-Stack Developer</h2>
+        <div className="hero__profession--line border__down"></div>
+      </section>
+    </>
   );
 };
 
