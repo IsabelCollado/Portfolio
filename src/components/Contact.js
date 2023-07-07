e10410a7e6c62df1657b7599136596ed53e32aeb
Isabel Collado Herrera <icolladoherrera@gmail.com>
2023-07-07 20:51:56 +0200

    "add project changes and modification in packege.json"

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,13 @@
-const contact = () => {};
+import '../styles/layouts/contact.scss';
+
+const contact = () => {
+  return (
+    <>
+      <section className="sectionContact">
+        <h1 className="sectionContact--h1">Contact</h1>
+      </section>
+    </>
+  );
+};
 
 export default contact;
