5a397ebbb0b1d18d0bd876ac207fb8d890264a47
IsabelCollado <icolladoherrera@gmail.com>
2023-07-10 21:07:19 +0200

    Add create page contact and style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,93 +6,108 @@ const contact = () => {
       <section className="sectionContact">
         <h1 className="sectionContact--h1">Contact</h1>
       </section>
-      <div className="projects-list">
-        <section className="contact container container-page">
-          <h2>Let's talk! 📞.</h2>
-          <p className="contact__text">
+      <div className="sectionContact--div">
+        <section>
+          <div className="hero__profession--line border__up"></div>
+          <h2 className="hero__profession">Let's talk! 📞 </h2>
+          <div className="hero__profession--line border__down"></div>
+        </section>
+        <section className="sectionContact--text">
+          <p className="sectionContact--text1">
             I am currently looking for a job. I would love to join a project
             that allows me to expand and sharpen my skills as a developer and
             help the team to develop their product. 📩
           </p>
-          <p className="contact__text">Puedes contactar conmigo al correo:</p>
-          <a className="contact__link" href="mailto:icolladoherrera@gmail.com">
+          <p className="sectionContact--text2">You can contact me by email:</p>
+          <a
+            className="sectionContact--link"
+            href="mailto:icolladoherrera@gmail.com"
+          >
             icolladoherrera@gmail.com
           </a>
-          <p className="contact__text">o a través de estos enlaces:</p>
-          <a
-            className="contact__link"
-            href="https://github.com/IsabelCollado"
-            alt="GitHub Isabel Collado"
-          >
-            <i className="fab fa-github-alt"></i>
-          </a>
-          <a
-            className="contact__link"
-            href="https://www.linkedin.com/in/nur-ahissami-1a7b24157/"
-            alt="Linkedin Nur Ahissami"
-          >
-            <i className="fab fa-linkedin-in"></i>
-          </a>
-          <a
-            className="contact__link"
-            href="https://www.instagram.com/nurahissami.design/"
-            alt="Instagram Nur Ahissami"
-          >
-            <i className="fab fa-instagram-square"></i>
-          </a>
-          <p className="contact__text">
-            O si lo prefieres, rellenando este formulario:
+          <p className="sectionContact--text3">
+            Or through the following links:
           </p>
-          <div className="containerform">
+          <div className="sectionLink--icons">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="sectionContact--link"
+              href="https://github.com/IsabelCollado"
+              alt="GitHub Isabel Collado"
+            >
+              <i className="fab fa-github-alt"></i>
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="sectionContact--link"
+              href="https://www.linkedin.com/in/isabel-collado-herrera/"
+              alt="Linkedin Isabel Collado"
+            >
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="sectionContact--link"
+              href="https://www.instagram.com/isaquini/"
+              alt="Instagram Isabel Collado"
+            >
+              <i className="fab fa-instagram-square"></i>
+            </a>
+          </div>
+          <p className="sectionContact--text4">
+            Or if you prefer, fill in this form:
+          </p>
+          <div className="sectionContact--divForm">
             <form
-              className="containerform__form"
-              action="https://getform.io/f/50572065-131e-42d8-a9db-1066cbd26b14"
+              className="sectionContact--form"
+              action="https://getform.io/f/ab69f5a0-d51f-4204-8c76-558c0a178293"
               method="POST"
             >
-              <label className="containerform__label" htmlFor="fname">
-                Nombre
+              <label className="sectionContact--label" htmlFor="fname">
+                Name
               </label>
               <input
-                className="containerform__input"
+                className="sectionContact--input"
                 type="text"
                 id="fname"
                 name="firstname"
-                placeholder="Tu nombre.."
+                placeholder="Name.."
               />
-              <label className="containerform__label" htmlFor="lname">
-                Apellido
+              <label className="sectionContact--label" htmlFor="lname">
+                Lastname
               </label>
               <input
-                className="containerform__input"
+                className="sectionContact--input"
                 type="text"
                 id="lname"
                 name="lastname"
-                placeholder="Apellido.."
+                placeholder="Lastname.."
               />
-              <label htmlFor="email" className="containerform__label">
+              <label htmlFor="email" className="sectionContact--label">
                 Email
               </label>
               <input
-                className="containerform__input"
+                className="sectionContact--input"
                 type="mail"
                 id="email"
                 name="email"
-                placeholder="tuemail@email.com"
+                placeholder="Youre-mail@email.com"
               />
-              <label htmlFor="subject" className="containerform__label">
-                Mensaje
+              <label htmlFor="subject" className="sectionContact--label">
+                Message
               </label>
               <textarea
-                className="containerform__textarea"
+                className="sectionContact--textarea"
                 id="subject"
                 name="subject"
-                placeholder="Escribe tu texto.."
+                placeholder="Write your own text.."
               />
-              <input
-                className="containerform__submit"
-                type="submit"
-                value="Enviar"
-              />
+              <div className="wrap">
+                <input className="button" type="submit" value="Submit" />
+              </div>
             </form>
           </div>
         </section>
