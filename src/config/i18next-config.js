7fa501450f02a90a87315c91bb17239f5b2cbe37
Isabel Collado Herrera <icolladoherrera@gmail.com>
2023-07-20 11:34:48 +0200

    "Add json spanish"

---
+++ src/config/i18next-config.js
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import aboutmeEN from '../languages/en/about.json';
+import headerEN from '../languages/en/header.json';
+import landingEN from '../languages/en/landing.json';
+import aboutmeES from '../languages/es/';
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        header: headerEN,
+        landing: landingEN,
+        aboutme: aboutmeEN,
+      },
+      es: {
+        header: headerES,
+        landing: landingES,
+        aboutme: aboutmeES,
+      },
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
